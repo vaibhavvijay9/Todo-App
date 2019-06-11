553e933ea706ab9427f18475a7b5f591f361c286
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2019-06-11 23:47:06 +0530

    checkbox functionality and dynamic class binding implemented correctly now. "done" key holds a boolean value and not string.

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ app.controller('TodoAppController', function($scope) {
     $scope.today = new Date();
 
     $scope.events = [
-        { text: 'learn node', done: 'true' },
-        { text: 'develop corner using MEAN', done: 'false' }
+        { text: 'learn node', done: true },
+        { text: 'develop corner using MEAN', done: false }
     ];
 
     // $scope.getAllEvents = function() {}
